7b444b552dabc9feeb96c8f1df1f6aaeefa5cd25
nabongsun <kangtu142@gmail.com>
2026-04-06 13:25:44 +0900

    Revert header text in AboutIntroduceSection component from "INTRODUCE222" back to "INTRODUCE" for consistency.

--- components/about/AboutIntroduceSection.tsx
+++ components/about/AboutIntroduceSection.tsx
@@ -7,7 +7,7 @@ const AboutIntroduceSection = () => {
         id="Introduce"
         className="flex flex-col gap-4 text-4xl font-bold mb-4"
       >
-        INTRODUCE222
+        INTRODUCE
       </div>
       <div className="border-l-2 border-gray-300 pl-4 md:ml-6 mb-8 space-y-8 text-2xl whitespace-pre-wrap">
         {Introduce.main}
